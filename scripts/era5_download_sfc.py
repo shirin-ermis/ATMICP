19c78e69f5945f1d115aab7a2517df6f76c42152
shirin-ermis <shirin.ermis@physics.ox.ac.uk>
2026-06-09 14:52:05 +0100

    era5 download scripts using cdsapi

---
+++ scripts/era5_download_sfc.py
@@ -0,0 +1,48 @@
+import cdsapi
+import numpy as np
+import glob
+import xarray as xr
+import os
+import subprocess
+
+var = 'sp'
+long_var = 'surface_pressure'
+savedir = f'/gf5/predict/AWH019_ERMIS_ATMICP/DATA/ERA5/{var}_monthly/' # make sure dir exists
+
+filename = f'{savedir}{var}_mon_ERA5_0.25x0.25_197901-202512.nc'
+dataset = "reanalysis-era5-single-levels-monthly-means"
+request = {
+    "product_type": ["monthly_averaged_reanalysis"],
+    "variable": [long_var],
+
+    "year": [ # full satellite era
+        "1979", "1980", "1981",
+        "1982", "1983", "1984",
+        "1985", "1986", "1987",
+        "1988", "1989", "1990",
+        "1991", "1992", "1993",
+        "1994", "1995", "1996",
+        "1997", "1998", "1999",
+        "2000", "2001", "2002",
+        "2003", "2004", "2005",
+        "2006", "2007", "2008",
+        "2009", "2010", "2011",
+        "2012", "2013", "2014",
+        "2015", "2016", "2017",
+        "2018", "2019", "2020",
+        "2021", "2022", "2023",
+        "2024", "2025"
+    ],
+    "month": [ # all months
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "time": ["00:00"], # 00:00 is standard for monthly means
+    "data_format": "netcdf", # makes request massive but oh well
+    "download_format": "unarchived"
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request, filename)
